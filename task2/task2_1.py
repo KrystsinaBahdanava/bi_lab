58e0302d38529405371019ce49724b2bb957af80
Krystsina Bahdanava <krystsina_bahdanava@epam.com>
2018-05-23 14:28:09 +0300

    Task2

--- task2/task2_1.py
+++ task2/task2_1.py
@@ -5,6 +5,6 @@ my_str = my_str.casefold()
 rev_str = my_str[::-1]
 
 if list(my_str) == list(rev_str):
-   print("It is palindrome")
+    print("It is palindrome")
 else:
-   print("It is not palindrome")
+    print("It is not palindrome")
--- task2/task2_4.py
+++ task2/task2_4.py
@@ -1,6 +1,7 @@
 def find_message(text):
     return "".join([c for c in text if c.isupper()])
 
+
 if __name__ == '__main__':
     assert find_message(u"How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
     assert find_message(u"hello world!") == "", "Nothing"
