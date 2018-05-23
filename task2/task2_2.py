d8c75342bc0c187f0e84f8067ef5a6041d5f3e6b
Krystsina Bahdanava <krystsina_bahdanava@epam.com>
2018-05-23 14:30:34 +0300

    Task2

--- task2/task2_2.py
+++ task2/task2_2.py
@@ -1,6 +1,6 @@
 amountD = (input("One item costs dollars:"))
 amountC = (input("and cents: "))
-amount = float(amountD+"."+amountC)
+amount = float(amountD + "." + amountC)
 quantity = int(input("Customer bought "))
 
 totalAm = float(amount * quantity)
