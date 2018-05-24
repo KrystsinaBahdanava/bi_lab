d8c77d0f733202ca56e348c77c63b33ef29b7dca
Krystsina Bahdanava <krystsina_bahdanava@epam.com>
2018-05-24 13:49:57 +0300

    Task3

--- task2/task2_2.py
+++
@@ -1,8 +0,0 @@
-amountD = (input("One item costs dollars:"))
-amountC = (input("and cents: "))
-amount = float(amountD + "." + amountC)
-quantity = int(input("Customer bought "))
-
-totalAm = amount * quantity
-
-print("Total is ", totalAm, "$")
--- task2/task2_4.py
+++
@@ -1,8 +0,0 @@
-def find_message(text):
-    return "".join([c for c in text if c.isupper()])
-
-
-if __name__ == '__main__':
-    assert find_message(u"How are you? Eh, ok.") == "HELLO", "hello"
-    assert find_message(u"hello world!") == "", "Nothing"
-    assert find_message(u"HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
